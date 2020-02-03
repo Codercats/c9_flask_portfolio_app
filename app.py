b5121e2dea73fb007d9f6d275fb4dff57e259715
Marc <bmarc4321@gmail.com>
2020-02-03 18:52:08 +0000

    Persuading users to leave feedback

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def inject_now():
 app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
 #app.run(debug=True)
