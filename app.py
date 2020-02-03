b4921e784030e0971fc0072df828566bb423b076
Marc <bmarc4321@gmail.com>
2020-02-03 23:27:55 +0000

    Added iframe preview

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def inject_now():
     return {'now': datetime.utcnow()}
 
 
-app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+#app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
 	app.run(debug=True)
